9761fce83c6eedaf28a5fb0a76d257d43acb125b
EvaLab900 <47912203+EvaLab900@users.noreply.github.com>
2019-03-02 13:26:03 +0200

    Add files via upload

---
+++ src/homework04/RandomGame.java
@@ -0,0 +1,57 @@
+package homework04;
+
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class RandomGame {
+	
+	public static void main(String[] args) {
+		
+		        int ActualNumber = 0;
+		        int UsersGuess = 0;
+		        int TriesCount = 0;
+		        Scanner in = new Scanner(System.in);
+
+		        System.out.println("Let's play! I picked a number from 0 to 100. Try to guess it: ");
+
+		        ActualNumber = 0 + (int)(random() * 100);
+
+		        do {
+		        	TriesCount++;
+					if (TriesCount > 5) {
+						System.out.println("You have run out of tries!");
+						break;
+					}
+
+					if (TriesCount != 1) {
+					System.out.println((6 - TriesCount) + "tries left");
+					}else {
+						System.out.println((6 - TriesCount) + "try left");
+					}
+					System.out.print("Enter your number: ");
+				
+					while(true) {
+						if (in.hasNextInt()) {
+							UsersGuess = in.nextInt();
+			
+							if (UsersGuess > ActualNumber) {
+								System.out.println("My number is smaller.");
+							} else if (UsersGuess < ActualNumber) {            	
+								System.out.println("My number is bigger.");	 		 		
+							} else { 	
+								System.out.println("You win !");
+							}
+						} else {
+							System.out.println("Please, try again.");
+							in.next();
+						}
+					}	
+		        } while (UsersGuess != ActualNumber );
+		        
+		        in.close();
+
+
+		    }
+
+	}
+
